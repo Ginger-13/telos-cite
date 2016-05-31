5d990f1ed3698a3ffbfbf23ce132144f187288ba
Ginger-13 <shepardcommander13.0@gmail.com>
2016-05-31 14:07:45 +0300

    top-link

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $('.js-slick').slick({
     rows: 2,
     slidesPerRow: 4
 });
+
+$('.on-top').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 2000);
+
+    return false;
+});
+
+$('a[href="#"]').on('click', function() {
+    return false;
+});
